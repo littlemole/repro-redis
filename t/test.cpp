8bb64e061434ad6393c504f1e5ff4e6e0c2d5cdf
little.mole <little.mole@oha7.org>
2018-10-22 21:37:09 +0200

    libs

--- t/test.cpp
+++ t/test.cpp
@@ -263,7 +263,7 @@ TEST_F(BasicTest, RawRedisSubscribe)
 
 int main(int argc, char **argv) {
 
-	prio::init();
+	prio::Libraries<prio::EventLoop> init;
 
     ::testing::InitGoogleTest(&argc, argv);
     int r = RUN_ALL_TESTS();
